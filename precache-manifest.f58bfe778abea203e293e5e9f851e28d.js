67b339ca5b8004951d69275c68387cef7f344347
Santosh <santosh.thorani@hotmail.com>
2024-01-14 22:53:22 +0100

    Updates

---
+++ precache-manifest.f58bfe778abea203e293e5e9f851e28d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1c34da1a970e3f69f58c1265841c37",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e01f0573627f5b6642e1",
+    "url": "/portfolio/static/css/2.01d557b6.chunk.css"
+  },
+  {
+    "revision": "d8ac9cfcb5ff30244297",
+    "url": "/portfolio/static/css/main.c0b12f1b.chunk.css"
+  },
+  {
+    "revision": "e01f0573627f5b6642e1",
+    "url": "/portfolio/static/js/2.4ce0423b.chunk.js"
+  },
+  {
+    "revision": "32f24742585caeac27f9ecc59ba74e5d",
+    "url": "/portfolio/static/js/2.4ce0423b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ac9cfcb5ff30244297",
+    "url": "/portfolio/static/js/main.850776a7.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "78f64963a1388fef7590e3a0d4a8d772",
+    "url": "/portfolio/static/media/2clicks.78f64963.png"
+  },
+  {
+    "revision": "460625aa86de918415f5f7110e021a49",
+    "url": "/portfolio/static/media/PokeRosterLight.460625aa.png"
+  },
+  {
+    "revision": "c868a845c4d21ce51c779062c24607f6",
+    "url": "/portfolio/static/media/ck.c868a845.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f47eb89b4808828241a852a549350d3d",
+    "url": "/portfolio/static/media/goto.f47eb89b.png"
+  },
+  {
+    "revision": "c689bd1d03023585df623ceed454b68a",
+    "url": "/portfolio/static/media/hbl.c689bd1d.png"
+  },
+  {
+    "revision": "10bb0ed55c69efc181842ec45a1e7547",
+    "url": "/portfolio/static/media/help.10bb0ed5.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "63707a70a1dec7a0c43e2f36c9d17ad0",
+    "url": "/portfolio/static/media/khaity.63707a70.png"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/portfolio/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/portfolio/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/portfolio/static/media/myImage.bd80794f.png"
+  },
+  {
+    "revision": "712d208057bacf9a9d40b6f133b08120",
+    "url": "/portfolio/static/media/shifaam-dr.712d2080.png"
+  },
+  {
+    "revision": "46eaa041fc47d20827dc122b8ff12459",
+    "url": "/portfolio/static/media/shifam-patient.46eaa041.png"
+  },
+  {
+    "revision": "e3508e20b2b34457ec7fb2368fcb4952",
+    "url": "/portfolio/static/media/talent-view.e3508e20.png"
+  },
+  {
+    "revision": "7f634e9a3e616f032fd89a161ac3aa68",
+    "url": "/portfolio/static/media/whosup.7f634e9a.png"
+  }
+]);
